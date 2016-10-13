5e56bc0251b087a76e845e58aba2c2ff0e856f37
ArtiIOMI <arti2895@gmail.com>
2016-10-13 21:30:20 +0200

    First stable version

---
+++ background.js
@@ -0,0 +1,60 @@
+﻿// Dane wlasciciela powiadamiacza
+var Strona = "http://www.hitbox.tv/losiugra";
+var StronaAPI = "http://api.hitbox.tv/user/losiugra";
+var Nazwa = 'Losiu';
+//
+
+function show() {
+	var time = /(..)(:..)/.exec(new Date());    
+	var hour = time[1] % 12 || 12;               
+	var period = time[1] < 12 ? 'a.m.' : 'p.m.'; 
+	var n = new Notification('Stream ('+hour + time[2] + ' ' + period+')', {
+		icon: 'data/img/icon_48.png',
+		body: Nazwa+' zaprasza na stream.'
+	});
+	var audio = new Audio("sound.mp3");
+	if (JSON.parse(localStorage.isMusic))
+	{
+		audio.volume= localStorage.glosnosc/100;
+		audio.play();
+	}
+	n.onclose = function(event) {audio.pause()}
+	n.onclick = function(event) {window.open(Strona, "_blank"); n.close()}
+}
+
+if (!localStorage.isInitialized) {
+  localStorage.isMusic = true;
+  localStorage.frequency = 1;
+  localStorage.glosnosc = 100;
+  localStorage.isInitialized = true;
+}
+
+var stream = false;
+
+if (window.Notification) {
+	var interval = 0;
+			$.getJSON( StronaAPI, function( api ) {
+				if(api.is_live == 1 && stream==false)
+				{
+					 show();
+					 stream=true;
+				}
+			 });
+
+	setInterval(function() {
+    interval++;
+
+	
+    if (localStorage.frequency <= interval) 
+	{		
+		$.getJSON( StronaAPI, function( api ) {
+			if(api.is_live == 1 && stream==false)
+			{
+				show();
+				stream=true;
+			}else if(api.is_live == 0 && stream==true) stream=false;
+		});
+		interval = 0;
+    }
+  }, 60000);
+}
---
+++ options.js
@@ -0,0 +1,17 @@
+﻿window.addEventListener('load', function() {
+  // Initialize the option controls.
+  options.isMusic.checked = JSON.parse(localStorage.isMusic);
+  options.frequency.value = localStorage.frequency;
+  options.glosnosc.value = localStorage.glosnosc;
+
+  options.isMusic.onchange = function() {
+    localStorage.isMusic = options.isMusic.checked;
+  };
+  
+  options.frequency.onchange = function() {
+    localStorage.frequency = options.frequency.value;
+  };  
+  options.glosnosc.onchange = function() {
+    localStorage.glosnosc = options.glosnosc.value;
+  };
+});
